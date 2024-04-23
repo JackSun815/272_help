74eab9e4d0120e292b5a88cf00f7d13f538cb2e6
JackSun815 <zhijians@andrew.cmu.edu>
2024-04-23 09:04:38 -0400

    merging two branches

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,8 +15,8 @@ const config: Config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'JackSun815', // Usually your GitHub org/user name.
+  projectName: '272_help', // Usually your repo name.
 
   onBrokenLinks: 'ignore',
   onBrokenMarkdownLinks: 'ignore',
