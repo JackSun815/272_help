b039044f593778bc13ddcaec7711ef0fcd1e4b6a
JackSun815 <zhijians@andrew.cmu.edu>
2024-04-24 18:06:53 -0400

    rebuild because display not showing

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,7 +8,7 @@ const config: Config = {
   favicon: 'img/Designer.png',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'http://jacksun815.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/272_help/',
